ad1b3e8e5c04b29c9e750f34cd55cc9e8a41f923
div-vik <shivamambast5@gmail.com>
2023-12-03 03:34:24 +0530

    Create table

--- sharpe/src/components/Table.jsx
+++ sharpe/src/components/Table.jsx
@@ -1,109 +1,46 @@
 import * as React from "react";
-import Paper from "@mui/material/Paper";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TablePagination from "@mui/material/TablePagination";
-import TableRow from "@mui/material/TableRow";
 
 const columns = [
-  { id: "id", label: "ID", minWidth: 170 },
-  { id: "title", label: "Title", minWidth: 100 },
-  {
-    id: "body",
-    label: "Body",
-    minWidth: 170,
-    align: "right",
-  },
+  { id: "id", header: "ID" },
+  { id: "title", header: "Title" },
+  { id: "body", header: "Body" },
 ];
 
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
-
-const rows = [
-  createData("India", "IN", 1324171354, 3287263),
-  createData("China", "CN", 1403500365, 9596961),
-  createData("Italy", "IT", 60483973, 301340),
-  createData("United States", "US", 327167434, 9833520),
-  createData("Canada", "CA", 37602103, 9984670),
-  createData("Australia", "AU", 25475400, 7692024),
-  createData("Germany", "DE", 83019200, 357578),
-  createData("Ireland", "IE", 4857000, 70273),
-  createData("Mexico", "MX", 126577691, 1972550),
-  createData("Japan", "JP", 126317000, 377973),
-  createData("France", "FR", 67022000, 640679),
-  createData("United Kingdom", "GB", 67545757, 242495),
-  createData("Russia", "RU", 146793744, 17098246),
-  createData("Nigeria", "NG", 200962417, 923768),
-  createData("Brazil", "BR", 210147125, 8515767),
-];
-
-export default function PostTable() {
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
-
+const Table = ({ posts }) => {
   return (
-    <Paper sx={{ width: "100%", overflow: "hidden" }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                    {columns.map((column) => {
-                      const value = row[column.id];
-                      return (
-                        <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === "number"
-                            ? column.format(value)
-                            : value}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        className="text-red-500"
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </Paper>
+    <>
+      <table className="w-full h-full">
+        <thead className="">
+          <tr className="sticky top-0 z-10 bg-[#09090B] border-b-[1px] border-gray-500">
+            {columns.map((column) => (
+              <th key={column.id} className="py-2 px-5 text-[#999999]">
+                {column.header}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody className=" overflow-y-scroll">
+          {posts.map((post) => {
+            const { id, title, body } = post;
+
+            return (
+              <tr key={id}>
+                <td className="px-5 text-[#999999] border-b-[1px] border-gray-500">
+                  {id}
+                </td>
+                <td className="px-5 text-[#999999] border-b-[1px] border-gray-500">
+                  {title}
+                </td>
+                <td className="px-5 text-[#999999] border-b-[1px] border-gray-500">
+                  {body}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
   );
-}
+};
+
+export default Table;
